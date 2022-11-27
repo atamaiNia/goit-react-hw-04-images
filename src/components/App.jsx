91944f50db30b3d7eedea7f69f023b9d71d29111
atamaiNia <bimatideka@gmail.com>
2022-11-27 18:21:55 +0200

    fixed mistake

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,9 +20,10 @@ export default function App() {
   const [showBtn, setShowBtn] = useState(false);
 
   useEffect(() => {
+    if (!query) {
+      return;
+    }
     (async () => {
-      if (query) {
-      }
       setShowLoader(true);
       setShowBtn(false);
 
